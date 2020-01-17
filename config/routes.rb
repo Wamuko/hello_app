8f8a80f691fb7d6ab1395adfa0ce8f642fa07a1f
Wamuko <wamuko.kazu@gmail.com>
2020-01-17 16:54:00 +0000

    ここまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  post 'signup', to: 'users#new'
+  get 'users/:id', to: 'users#show'
+  patch 'users/:id', to: 'users#update'
+  post 'close', to: 'users#destroy'
 end
